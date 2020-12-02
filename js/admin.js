a63e8e9c6bc780321f4dc261f5ee27a6f84acf84
Ben Johnston <benj@princeton.edu>
2020-12-02 12:58:05 -0500

    changes

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,5 @@
 jQuery( document ).ready(function() {
 
-console.log(leafletvars);
-
 
 	/*************************
 	* check if map data is active
--- lib/settings.php
+++ lib/settings.php
@@ -5,7 +5,13 @@
  * Adds link to settings admin menu
  */
  
+
+ 
+ 
+ 
+ 
 add_action( 'admin_menu', 'puleafletmaps_custom_admin_menu' ); 
+
  
 function puleafletmaps_custom_admin_menu() {
     add_options_page(
@@ -15,20 +21,69 @@ function puleafletmaps_custom_admin_menu() {
         'link-types',
         'puleafletmaps_options_page'
     );
+    
+    add_action( 'admin_init', 'register_puleafletmaps_settings' );  
 }
+
+
+function register_puleafletmaps_settings() {
+	register_setting( 'leaflet_maps_settings', 'leaflet_maps_basemap' );
+	register_setting( 'leaflet_maps_settings', 'leaflet_maps_overlay_image' );
+	register_setting( 'leaflet_maps_settings', 'leaflet_maps_overlay_coords' );
+	register_setting( 'leaflet_maps_settings', 'leaflet_maps_postpage' );
+	register_setting( 'leaflet_maps_settings', 'leaflet_maps_show' );
+}
+
 
 
 /********************************
  * Prints out the settings page
  ********************************/
 function puleafletmaps_options_page() {
+
+	$basemaps = array(
+	   "OpenStreetMaps Streets" => "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+	   "Stamen Toner" => "//{s}.tile.stamen.com/toner/{z}/{x}/{y}.png",
+	); 
+
+
+
+  if ( !current_user_can( 'manage_options' ) )  {
+ 	wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+  }
+  
+  if(!$basemap = get_option('leaflet_maps_basemap')) {  $basemap = ""; }
+  if(!$overlay_image = get_option('leaflet_maps_overlay_image')) {  $overlay_image = ""; }
+  if(!$overlay_coords = get_option('leaflet_maps_overlay_coords')) {  $overlay_coords = ""; }
+  if(!$postpage = get_option('leaflet_maps_postpage')) {  $postpage = array('',''); }
+  if(!$show = get_option('leaflet_maps_show')) {  $show = 0; }
  ?>
- <div class='wrap'>
+   <div class='wrap'>
    <h1>Leaflet Maps</h1>
-   <p>Base Map:</p>
-   <p>Overlay:</p>
-   <p>Allow maps on: <input type='checkbox' checked='checked'/> Posts <input type='checkbox'/> Pages</p>
-   <p>Show map on single blog post pages:</p>
+   <form method="post" action="options.php"> 
+   <?php 
+     settings_fields( 'leaflet_maps_settings' ); 
+     do_settings_sections( 'leaflet_maps_settings' );
+   ?>
+     <p><label for='basemap'>Base Map:</label>
+      <input class='regular-text' type='text' id='basemap' name='leaflet_maps_basemap' value='<?php echo $basemap; ?>'/>
+     </p>
+     <p>//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png</p>
+     <p>//{s}.tile.stamen.com/toner/{z}/{x}/{y}.png</p>
+     <p><label for='overlay'>Overlay Image:</label> <input class='regular-text' type='text' id='overlay_image' name='leaflet_maps_overlay_image' value='<?php echo $overlay_image; ?>'/></p>
+     <p><label for='overlay'>Overlay coordinates:</label> <input class='regular-text' type='text' id='overlay_coords' name='leaflet_maps_overlay_coords' value='<?php echo $overlay_coords; ?>'/></p>
+          
+     <p>Allow maps on: <input type='checkbox' name='leaflet_maps_postpage[]' <?php if($postpage[0]==1) { echo "checked='checked'";}?> value='1'/> Posts <input type='checkbox'  name='leaflet_maps_postpage[]' <?php if($postpage[0]==1) { echo "checked='checked'";}?> value='1'/> Pages</p>
+     <p><input type='checkbox' name='leaflet_maps_show' <?php if($show==1) { echo "checked='checked'";}?> value='1'/> Show map on single blog post pages</p>
+     <p class="submit">
+	<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+	</p>
+   </form>
  </div>
   <?php
 }
+
+
+
+
+
